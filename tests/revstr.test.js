b6c61b08e2d84a648cce53cd302640ad99b9080d
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-08-24 01:59:28 +0100

    add more tests

--- tests/revstr.test.js
+++ tests/revstr.test.js
@@ -2,11 +2,11 @@ const formel = require("../build/index");
 
 
 it('reverse a string', () => {
-  expect(formel("123456789").reverseStr().val)
+  expect(formel("123456789").revstr().val)
   .toBe("987654321");
 });
 
 it('reverse a string', () => {
-  expect(formel("hello world").reverseStr().val)
+  expect(formel("hello world").revstr().val)
   .toBe("dlrow olleh");
 });
